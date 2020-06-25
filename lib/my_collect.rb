8a710e58f22ec03c6c15ba55e2b40339046063f6
Sean Scamehorn <smscamehorn45@gmail.com>
2020-06-25 22:37:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ array = ["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"]
 
 def my_collect(array)
 i = 0 
-name_collection = []
+name_collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 while i < array.length 
 name_collection.pop
 
